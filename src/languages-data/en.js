1611589855a2166c3ffec51de4ffb0244d5c8c87
Fabio <kyniexpro@wp.pl>
2021-01-12 16:07:27 +0100

    Update en.js

--- src/languages-data/en.js
+++ src/languages-data/en.js
@@ -454,7 +454,7 @@ To know more about the project, check out the videos below.
 </div>
 </li>
 <li>
-<a class="cd-faq-trigger" href="#0">Will there be focus stones in fashion?</a>
+<a class="cd-faq-trigger" href="#0">Will there be focus stones in modification?</a>
 <div class="cd-faq-content">
 <p>Yes, but we don't want to reveal any details about it.</p>
 </div>
